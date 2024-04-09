709869064ee143bd880458dac59f4b079a003141
ForzenString <964413011@qq.com>
2024-04-09 19:17:16 +0800

    :sparkles: 添加家长注册接口

--- src/apiRequest/parent/authorize/login.ts
+++ src/apiRequest/parent/authorize/login.ts
@@ -3,13 +3,22 @@ import client from "../../baseRequest.ts";
 const uri = "/api/v0/parent/login"
 
 
-export async function parentLogin(parentId:string,password:string){
-    let resp = await client.post<string>(uri,{
-        unique_id:parentId,
-        pwd:password
-    },{
-        method:"POST",
+export async function parentLogin(parentId: string, password: string) {
+    let resp = await client.post<string>(uri, {
+        unique_id: parentId,
+        pwd: password
+    }, {
+        method: "POST",
     })
 
     return resp.data
+}
+
+export async function register(name: string, password: string, uniqueId: string, secret: string) {
+    await client.post<void>("/api/v0/parent/register", {
+        name,
+        "id": uniqueId,
+        password,
+        secret
+    });
 }
