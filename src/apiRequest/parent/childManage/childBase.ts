bdf5e336aac7cd5cdd2f2b9028153ff7df4f0411
ForzenString <964413011@qq.com>
2024-04-09 19:14:30 +0800

    :sparkles: 添加创建孩子/去除孩子接口

--- src/apiRequest/parent/childManage/childBase.ts
+++ src/apiRequest/parent/childManage/childBase.ts
@@ -6,12 +6,28 @@ export interface ChildBase {
     name: string;
 }
 
-export async function getChildBaseInfo(auth: string, childId: number){
+export async function getChildBaseInfo(auth: string, childId: number) {
     const resp = await client.get<ChildBase>("/api/v0/parent/children", {
         method: "GET", headers: {
             "Authorization": auth,
-        },params:{cid:childId}
+        }, params: {cid: childId}
     })
 
     return resp.data
+}
+
+
+export async function createChild(auth: string, childName: string) {
+    const resp = await client.post<number>("/api/v0/parent/children",
+        {name: childName}
+        , {headers: {Authorization: auth,}}
+    );
+    return resp.data
+}
+
+export async function deleteChild(auth: string, childId: number) {
+    await client.delete("/api/v0/parent/children", {
+        headers: {Authorization: auth,},
+        data: {cid: childId}
+    })
 }
