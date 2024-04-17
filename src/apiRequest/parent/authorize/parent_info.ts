1c60054449a4f24fdc7778dd73df210371450cf7
ForzenString <964413011@qq.com>
2024-04-17 08:55:19 +0800

    :sparkles: 添加家长姓名显示

--- src/apiRequest/parent/authorize/parent_info.ts
+++ src/apiRequest/parent/authorize/parent_info.ts
@@ -1,1 +1,14 @@
+import client, {ResponseResult} from "../../baseRequest.ts";
 
+export async function getParentName(auth: string): Promise<ResponseResult<string>> {
+    const resp = await client.get<string>("/api/v0/parent/name",
+        {
+            headers: {
+                Authorization: auth
+            }
+        }
+    )
+
+    return resp.data
+
+}
--- src/pages/parent/ParentPage.vue
+++ src/pages/parent/ParentPage.vue
@@ -1,21 +1,36 @@
 <script lang="ts" setup>
 import {useRouter} from "vue-router";
 import AuthorizeManager from "@/utils/authorize.ts";
+import {onMounted, ref} from "vue";
+import {getParentName} from "@/apiRequest/parent/authorize/parent_info.ts";
+import {ResponseResult} from "@/apiRequest/baseRequest.ts";
 
 const router = useRouter();
-
+const parentName = ref<string>("x")
 
 // callback
 const logout = () => {
   AuthorizeManager.clearToken();
   router.push("/")
 }
+
+onMounted(()=>{
+  getParentName(AuthorizeManager.getToken())
+      .then((resp:ResponseResult<string>)=>{
+        parentName.value=resp.expect()
+      })
+})
+
 </script>
 
 <template>
   <v-app-bar color="primary" elevation="2">
     <template #prepend>
-      <v-avatar color="orange">A</v-avatar>
+      <v-col>
+
+      <v-avatar color="deep-purple-accent-4">{{ parentName.length > 0 ? parentName[0] : "-"}}</v-avatar>
+      <span class="ms-4">{{parentName}} 欢迎回来</span>
+      </v-col>
     </template>
 
     <template #title>家长管理</template>
